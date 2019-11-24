adeb277285e7f66db0761af40bf2e580cd7a05a7
Karthikeyan Marudhachalam <karthikeyan.marudhachalam@freshworks.com>
2019-11-24 15:50:14 +0530

    Replaced ModelMapper with Mapstruct for samples/api module.

--- samples/api/src/main/java/com/freshworks/starter/sample/api/controller/TodoController.java
+++ samples/api/src/main/java/com/freshworks/starter/sample/api/controller/TodoController.java
@@ -1,9 +1,9 @@
 package com.freshworks.starter.sample.api.controller;
 
 import com.freshworks.starter.sample.api.dto.TodoDto;
+import com.freshworks.starter.sample.api.mapper.TodoMapper;
 import com.freshworks.starter.sample.common.model.Todo;
 import com.freshworks.starter.sample.common.service.TodoService;
-import org.modelmapper.ModelMapper;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,45 +15,38 @@ import java.util.stream.Collectors;
 @RequestMapping(path = "/api/v1/todos")
 public class TodoController {
     private final TodoService todoService;
-    private ModelMapper modelMapper;
+    private TodoMapper todoMapper;
 
-    public TodoController(TodoService todoService, ModelMapper modelMapper) {
+    public TodoController(TodoService todoService, TodoMapper todoMapper) {
         this.todoService = todoService;
-        this.modelMapper = modelMapper;
+        this.todoMapper = todoMapper;
     }
 
     @GetMapping
     @PreAuthorize("hasAuthority('/todo/list')")
     public List<TodoDto> listTodos() {
         List<Todo> todos = todoService.listAllTodos();
-        return todos.stream().map(this::convertToDto).collect(Collectors.toList());
+        return todos.stream().map(t -> todoMapper.convert(t)).collect(Collectors.toList());
     }
 
     @PostMapping
     @PreAuthorize("hasAuthority('/todo/create')")
     public TodoDto addTodo(@RequestBody @Valid TodoDto newTodoDto) {
-        Todo todo = convertToEntity(newTodoDto);
-        return convertToDto(todoService.addTodo(todo));
+        Todo todo = todoMapper.convert(newTodoDto);
+        return todoMapper.convert(todoService.addTodo(todo));
     }
 
     @PutMapping("/{todoId}")
     @PreAuthorize("hasAuthority('/todo/update')")
     public TodoDto updateTodo(@PathVariable long todoId, @RequestBody @Valid TodoDto updateTodoDto) {
         updateTodoDto.setId(todoId);
-        return convertToDto(todoService.updateTodo(convertToEntity(updateTodoDto)));
+        return todoMapper.convert(todoService.updateTodo(todoMapper.convert(updateTodoDto)));
     }
 
     @GetMapping("/{todoId}")
     @PreAuthorize("hasAuthority('/todo/get')")
     public TodoDto getTodo(@PathVariable long todoId) {
-        return convertToDto(todoService.getTodo(todoId));
-    }
-
-    private TodoDto convertToDto(Todo todo) {
-        return modelMapper.map(todo, TodoDto.class);
-    }
-    private Todo convertToEntity(TodoDto todoDto) {
-        return modelMapper.map(todoDto, Todo.class);
+        return todoMapper.convert(todoService.getTodo(todoId));
     }
 
 }
