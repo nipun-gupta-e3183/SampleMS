b2e48265932834792dd4c9990d8178e1d1e2c0de
Karthikeyan Marudhachalam <karthikeyan.marudhachalam@freshworks.com>
2019-11-25 09:53:14 +0530

    Introduced Lombok in samples/common module.

--- samples/common/src/main/java/com/freshworks/starter/sample/common/model/Todo.java
+++ samples/common/src/main/java/com/freshworks/starter/sample/common/model/Todo.java
@@ -1,74 +1,22 @@
 package com.freshworks.starter.sample.common.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
-import java.util.Objects;
 
 @Entity
 @Table(name = "TODOS")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String title;
     private boolean completed;
-
-    public Todo() {
-    }
-
-    public Todo(String title) {
-        this.title = title;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public boolean isCompleted() {
-        return completed;
-    }
-
-    public void setCompleted(boolean completed) {
-        this.completed = completed;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Todo)) return false;
-
-        Todo todo = (Todo) o;
-
-        if (id != todo.id) return false;
-        if (completed != todo.completed) return false;
-        return Objects.equals(title, todo.title);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = (int) (id ^ (id >>> 32));
-        result = 31 * result + (title != null ? title.hashCode() : 0);
-        result = 31 * result + (completed ? 1 : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Todo{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", completed=" + completed +
-                '}';
-    }
 }
