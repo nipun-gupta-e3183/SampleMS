c1970cb53768096bf119f2b9de10a47c0c9fe5ec
Karthikeyan Marudhachalam <karthikeyan.marudhachalam@freshworks.com>
2019-12-11 16:47:29 +0530

    SQS listener changed to delete the message on successful processing.

--- samples/sqs-processor/src/main/java/com/freshworks/starter/sample/sqs_processor/processor/TodoProcessor.java
+++ samples/sqs-processor/src/main/java/com/freshworks/starter/sample/sqs_processor/processor/TodoProcessor.java
@@ -4,6 +4,7 @@ import com.freshworks.starter.sample.common.service.TodoService;
 import com.freshworks.starter.sample.sqs_processor.dto.TodoDto;
 import com.freshworks.starter.sample.sqs_processor.mapper.TodoMapper;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class TodoProcessor {
         this.todoMapper = todoMapper;
     }
 
-    @SqsListener("todos")
+    @SqsListener(value = "todos", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
     public void process(TodoDto todoDto) {
         todoService.addTodo(todoMapper.convert(todoDto));
     }
