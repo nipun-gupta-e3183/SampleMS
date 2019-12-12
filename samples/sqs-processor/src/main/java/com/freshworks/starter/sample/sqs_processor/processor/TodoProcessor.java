ad8c49ef20589fd3cd6380513f9317459ef2a9cc
Karthikeyan Marudhachalam <karthikeyan.marudhachalam@freshworks.com>
2019-12-12 15:07:27 +0530

    Made topic list configurable via application.properties in SQS processor sample app.

--- samples/sqs-processor/src/main/java/com/freshworks/starter/sample/sqs_processor/processor/TodoProcessor.java
+++ samples/sqs-processor/src/main/java/com/freshworks/starter/sample/sqs_processor/processor/TodoProcessor.java
@@ -3,16 +3,12 @@ package com.freshworks.starter.sample.sqs_processor.processor;
 import com.freshworks.starter.sample.common.service.TodoService;
 import com.freshworks.starter.sample.sqs_processor.dto.TodoDto;
 import com.freshworks.starter.sample.sqs_processor.mapper.TodoMapper;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TodoProcessor {
-    @Value("sqs.queueName")
-    private String queueName;
-
     private final TodoService todoService;
     private TodoMapper todoMapper;
 
@@ -21,7 +17,7 @@ public class TodoProcessor {
         this.todoMapper = todoMapper;
     }
 
-    @SqsListener(value = "todos", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
+    @SqsListener(value = "${sqs.queueName}", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
     public void process(TodoDto todoDto) {
         todoService.addTodo(todoMapper.convert(todoDto));
     }
