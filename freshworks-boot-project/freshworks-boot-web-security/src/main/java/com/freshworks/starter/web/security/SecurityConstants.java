23960f888df6c5513907961a0984fb4b0475d505
Karthikeyan Marudhachalam <karthikeyan.marudhachalam@freshworks.com>
2019-12-17 21:34:50 +0530

    Deleted everything except samples directory.

--- freshworks-boot-project/freshworks-boot-web-security/src/main/java/com/freshworks/starter/web/security/SecurityConstants.java
+++
@@ -1,8 +0,0 @@
-package com.freshworks.starter.web.security;
-
-interface SecurityConstants {
-    long EXPIRATION_TIME = 60_000; // 1 minute
-    String TOKEN_PREFIX = "Bearer ";
-    String HEADER_STRING = "Authorization";
-}
-
