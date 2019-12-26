4a6297aae75d008982acf2038758bef2ccd00205
Karthikeyan Marudhachalam <karthikeyan.marudhachalam@freshworks.com>
2019-12-26 12:48:55 +0530

    Improved Kafka processor to receive parsed key & parse the payload based on payload_type.

--- samples/kafka-processor/src/main/java/com/freshworks/starter/sample/kafka_processor/config/KafkaConsumerConfig.java
+++ samples/kafka-processor/src/main/java/com/freshworks/starter/sample/kafka_processor/config/KafkaConsumerConfig.java
@@ -1,6 +1,5 @@
 package com.freshworks.starter.sample.kafka_processor.config;
 
-import com.freshworks.starter.sample.kafka_processor.dto.TodoDto;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
@@ -9,25 +8,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.Map;
 
 @Configuration
 public class KafkaConsumerConfig {
     @Bean
-    public ConsumerFactory<String, TodoDto> todoConsumerFactory(KafkaProperties properties) {
+    public ConsumerFactory<MessageKey, String> todoConsumerFactory(KafkaProperties properties) {
         Map<String, Object> config = properties.buildConsumerProperties();
-        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(),
-                new JsonDeserializer<>(TodoDto.class));
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, MessageKeyDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(config, new MessageKeyDeserializer(), new StringDeserializer());
     }
 
     // Note: One bean should be named "kafkaListenerContainerFactory" as spring-boot requires this.
     @Bean(name="kafkaListenerContainerFactory")
-    public ConcurrentKafkaListenerContainerFactory<String, TodoDto> todoKafkaListenerFactory(KafkaProperties properties) {
-        ConcurrentKafkaListenerContainerFactory<String, TodoDto> factory = new ConcurrentKafkaListenerContainerFactory<>();
+    public ConcurrentKafkaListenerContainerFactory<MessageKey, String> todoKafkaListenerFactory(KafkaProperties properties) {
+        ConcurrentKafkaListenerContainerFactory<MessageKey, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(todoConsumerFactory(properties));
         return factory;
     }
--- samples/kafka-processor/src/main/java/com/freshworks/starter/sample/kafka_processor/processor/TodoProcessor.java
+++ samples/kafka-processor/src/main/java/com/freshworks/starter/sample/kafka_processor/processor/TodoProcessor.java
@@ -1,13 +1,21 @@
 package com.freshworks.starter.sample.kafka_processor.processor;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.freshworks.starter.sample.common.service.TodoService;
+import com.freshworks.starter.sample.kafka_processor.config.MessageKey;
 import com.freshworks.starter.sample.kafka_processor.dto.TodoDto;
 import com.freshworks.starter.sample.kafka_processor.mapper.TodoMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class TodoProcessor {
+    private final Logger logger = LoggerFactory.getLogger(TodoProcessor.class);
     private final TodoService todoService;
     private TodoMapper todoMapper;
 
@@ -17,8 +25,17 @@ public class TodoProcessor {
     }
 
     @KafkaListener(topics = "${kafka.topics}", groupId = "${kafka.consumerGroupId}", containerFactory = "kafkaListenerContainerFactory")
-    public void process(TodoDto todoDto) {
-        todoService.addTodo(todoMapper.convert(todoDto));
+    public void process(String data, @Header(name = "kafka_receivedMessageKey") MessageKey key) {
+        if ("todo_create".equals(key.getPayloadType())) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            TodoDto todoDto = null;
+            try {
+                todoDto = objectMapper.readValue(data, TodoDto.class);
+                todoService.addTodo(todoMapper.convert(todoDto));
+            } catch (IOException e) {
+                logger.warn("Couldn't parse message with key: {}", key);
+            }
+        }
     }
 
 }
