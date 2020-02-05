371876085b9394f4e4378102b353e4e642581d68
Vignesh Jagadeesh <vignesh.jagadeesh@freshworks.com>
2020-02-05 11:40:01 +0530

    Sonarqube Integration

--- samples/scripts/build-app/jenkinsfile.groovy
+++ samples/scripts/build-app/jenkinsfile.groovy
@@ -1,3 +1,32 @@
+def sonarqube_analysis() {
+    stage('SonarQube Analysis') {
+        withCredentials([string(credentialsId: 'SonarqubeGithubToken', variable: 'SonarqubeGithubToken')]) {
+            withSonarQubeEnv('FreshdeskSonarqube') {
+
+                if (env.ghprbPullId) {
+                    sh """
+                    mvn sonar:sonar \
+                    -Dsonar.projectKey=${env.sonar.projectKey} \
+                    -Dsonar.github.repository=${env.sonar.repository} \
+                    -Dsonar.pullrequest.key=${env.ghprbPullId} \
+                    -Dsonar.pullrequest.branch=${env.ghprbSourceBranch} \
+                    -Dsonar.pullrequest.base=${env.ghprbTargetBranch} \
+                    -Dsonar.github.oauth=${SonarqubeGithubToken}
+                   """
+                } else {
+                    sh """
+                    mvn sonar:sonar \
+                    -Dsonar.projectKey=${env.sonar.projectKey} \
+                    -Dsonar.github.repository=${env.sonar.repository} \
+                    -Dsonar.github.oauth=${SonarqubeGithubToken}
+                   """
+                }
+            }
+        }
+    }
+}
+
+
 node('fd-jenkins-slave-default') {
     stage('Checkout') {
         sh "rm -rf $WORKSPACE/*"
@@ -11,6 +40,7 @@ node('fd-jenkins-slave-default') {
         stage('Build') {
             docker.image('maven:3-jdk-11').inside {
                 sh("export MAVEN_CONFIG=''; cp mvnsettings.xml /root/.m2/settings.xml && ./mvnw clean install")
+                sonarqube_analysis()
             }
         }
         post {
